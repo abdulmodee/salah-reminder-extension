699c423b682126a5f02b061b1b026db078b43521
Abdurrahman Modibbo <abdulrahmanmodibbo85@gmail.com>
2024-04-20 19:00:10 +0800

    commit2

--- project.js
+++ project.js
@@ -1,28 +1,35 @@
-function errorCallBack(error) {
-    console.error(error)
-}
 
-// Request user's location when they click the button
-document.getElementById("getLocationButton").addEventListener("click", function () {
-    // Check if geolocation is available in the browser
-    if ("geolocation" in navigator) {
-        // Request the user's location
-        const watchID = navigator.geolocation.watchPosition(fetchData, errorCallBack);
-    }
-});
-
+// Function to fetch Prayer Times Data from Al- Adhaan API
 async function fetchData(position) {
     const coordinates = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
     };
-    const res = await fetch(`http://api.aladhan.com/v1/timings?latitude=${coordinates.lat}&longitude=${coordinates.lng}`);
-    const record = await res.json();
-    document.getElementById("Fajr").innerHTML = record.data.timings.Fajr
-    document.getElementById("Sunrise").innerHTML = record.data.timings.Sunrise
-    document.getElementById("Dhuhr").innerHTML = record.data.timings.Dhuhr
-    document.getElementById("Asr").innerHTML = record.data.timings.Asr
-    document.getElementById("Maghrib").innerHTML = record.data.timings.Maghrib
-    document.getElementById("Isha").innerHTML = record.data.timings.Isha
+    const reponse = await fetch(`https://api.aladhan.com/v1/timings?latitude=${coordinates.lat}&longitude=${coordinates.lng}`);
+    const record = await reponse.json();
+    document.getElementById("Fajr").innerHTML = record.data.timings.Fajr;
+    document.getElementById("Sunrise").innerHTML = record.data.timings.Sunrise;
+    document.getElementById("Dhuhr").innerHTML = record.data.timings.Dhuhr;
+    document.getElementById("Asr").innerHTML = record.data.timings.Asr;
+    document.getElementById("Maghrib").innerHTML = record.data.timings.Maghrib;
+    document.getElementById("Isha").innerHTML = record.data.timings.Isha;
+
+    const btnReminder = document.getElementById("button-inner");
+    btnReminder.addEventListener("click", () => {
+        chrome.runtime.sendMessage({ "timings": record.data.timings }, function (response) {
+            console.log(response)
+        })
+        btnReminder.style.display = "none";
+    })
+
 }
-fetchData();
+
+
+if ("geolocation" in navigator) {
+    // Request the user's location
+    navigator.geolocation.getCurrentPosition(fetchData,
+        function (error) {
+            console.error(error);
+        }
+    );
+}
